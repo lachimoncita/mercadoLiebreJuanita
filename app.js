5a551c8a9dfe8d8a76fb1095b805f252cf3c1bac
lachimoncita <juanacolaneri@hotmail.com>
2022-09-23 19:21:39 -0300

    hola

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require ("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,"/public")));
+
+app.get("/", (req,res) => {
+    res.sendFile (path.join(__dirname, "/views/home.html"));
+    }
+);
+
+app.get("/views/register.html", (req,res) => {
+    res.sendFile (path.join(__dirname, "/views/register.html"));
+    }
+);
+
+app.get("/views/login.html", (req,res) => {
+    res.sendFile (path.join(__dirname, "/views/login.html"));
+    }
+);
+
+app.listen(3005, () => console.log("Servidor corriendo"));
+
+
